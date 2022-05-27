ccf519e47e158b3566e99fdfeafce5589de02cec
aditya6957 <99235984+aditya6957@users.noreply.github.com>
2022-05-27 14:58:47 +0530

    Add files via upload

---
+++ sampleproject_job.py
@@ -0,0 +1,27 @@
+'''
+sampleproject_job.py
+
+'''
+# see https://pubhub.devnetcloud.com/media/pyats/docs/easypy/jobfile.html
+# for how job files work
+
+# optional author information
+# (update below with your contact information if needed)
+__author__ = 'Cisco Systems Inc.'
+__copyright__ = 'Copyright (c) 2019, Cisco Systems Inc.'
+__contact__ = ['pyats-support-ext@cisco.com']
+__credits__ = ['list', 'of', 'credit']
+__version__ = 1.0
+
+import os
+from pyats.easypy import run
+from genie.harness.main import gRun
+
+def main():
+    gRun(
+	pdb=False,
+	TriggerGetRunningConfig="sampleproject_data.yaml",
+	trigger_uids=[
+	    "TriggerGetRunningConfig"
+	]
+	)
